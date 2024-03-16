2bd15795c4d4ea6842d4ed61a657a6062faac2d6
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-03-16 21:46:30 +0700

    🧹 chore: savepoint

--- Buoi_4/Home/Bai_1.cpp
+++ Buoi_4/Home/Bai_1.cpp
@@ -22,36 +22,37 @@ public:
     cArray();
     void NhapMang();
     void XuatMang() const;
-    int TimSoAmLonNhat() const;
+    void TaoNgauNhien();
     int DemLanXuatHienX(const int& x) const;
-    bool KiemTraGiamDan() const;
+    bool KiemTraTangDan() const;
+    int TimLeNhoNhat() const;
     void SapXepTangDan();
 };
 
 int main()
 {
-    // Nhap mang
     cArray myArray;
 
+    // Nhap mang
+    myArray.NhapMang();
+
+    // Xuat mang
+    cout << "Xuat mang da nhap" << endl;
     myArray.XuatMang();
 
-    int maxNeg = myArray.TimSoAmLonNhat();
-    switch (maxNeg)
-    {
-    case 0:
-        cout << "Khong co so am" << endl;
-        break;
-    default:
-        cout << "So am lon nhat: " << maxNeg << endl;
-        break;
-    }
+    // Tao ngau nhien
+    cout << "Tao mang ngau nhien" << endl;
+    myArray.TaoNgauNhien();
+    myArray.XuatMang();
 
+    // Dem so lan xuat hien x trong mang
     int x;
     cout << "Nhap x: ";
     cin >> x;
     cout << "So lan xuat hien " << x << ": " << myArray.DemLanXuatHienX(x) << endl;
 
-    switch (myArray.KiemTraGiamDan())
+    // Kiem tra tang dan
+    switch (myArray.KiemTraTangDan())
     {
     case false:
         cout << "Mang khong phai giam dan" << endl;
@@ -61,6 +62,19 @@ int main()
         break;
     }
 
+    // Tim phan tu le nho nhat
+    int minOdd = myArray.TimLeNhoNhat();
+    switch (minOdd)
+    {
+    case 0:
+        cout << "Khong co phan tu le nao" << endl;
+        break;
+    default:
+        cout << "Phan tu le nho nhat trong mang: " << minOdd << endl;
+        break;
+    }
+
+    // Sap xep tang dan
     cout << "Mang sap xep tang dan" << endl;
     myArray.SapXepTangDan();
     myArray.XuatMang();
@@ -68,18 +82,18 @@ int main()
     return 0;
 }
 
-cArray::cArray()
-{
-}
+cArray::cArray() {}
 
 void cArray::NhapMang()
 {
     cout << "Nhap n: ";
     cin >> this->n;
-    srand(time(NULL));
     this->v.resize(n);
-    for (int i = 0; i < n; i++)
-        v[i] = rand() % (MAX - MIN) - MAX + 1;
+    for (int i = 0; i < this->n; i++)
+    {
+        cout << "\tNhap v[" << i << "]: ";
+        cin >> this->v[i];
+    }
 }
 
 void cArray::XuatMang() const
@@ -89,22 +103,11 @@ void cArray::XuatMang() const
     cout << endl;
 }
 
-int cArray::TimSoAmLonNhat() const
+void cArray::TaoNgauNhien()
 {
-    int res = 0;
-    for (auto it : this->v)
-        if (it < 0)
-            switch (res)
-            {
-            case 0:
-                res = it;
-                break;
-            default:
-                if (it > res)
-                    res = it;
-                break;
-            }
-    return res;
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+        v[i] = rand() % (MAX - MIN) - MAX + 1;
 }
 
 int cArray::DemLanXuatHienX(const int& x) const
@@ -116,12 +119,30 @@ int cArray::DemLanXuatHienX(const int& x) const
     return count;
 }
 
-bool cArray::KiemTraGiamDan() const
+bool cArray::KiemTraTangDan() const
 {
     for (int i = 0; i < this->n - 1; i++)
-        if (v[i] < v[i + 1])
+        if (v[i] > v[i + 1])
             return false;
     return true;
+}
+
+int cArray::TimLeNhoNhat() const
+{
+    int res = 0;
+    for (auto it : this->v)
+        if (abs(it) % 2 == 1)
+            switch (res)
+            {
+            case 0:
+                res = it;
+                break;
+            default:
+                if (it < res)
+                    res = it;
+                break;
+            }
+    return res;
 }
 
 void cArray::SapXepTangDan()
