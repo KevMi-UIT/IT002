ea2ca5d7ba0234b86076599ff1bfc83372e64c1f
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-03-28 08:02:11 +0700

    sth

--- Buoi_6/Home/Bai_1.cpp
+++ Buoi_6/Home/Bai_1.cpp
@@ -4,12 +4,20 @@ using namespace std;
 class ComplexNum
 {
   private:
-    int real;
-    int imagine;
+    double real;
+    double imagine;
 
   public:
-    ComplexNum() : real{0}, imagine(0){};
-    ComplexNum(const int &, const int &);
+    ComplexNum() : real{0}, imagine{0} {};
+    ComplexNum(const double &, const double &);
+    ComplexNum operator-();
+    ComplexNum operator+(const ComplexNum &) const;
+    ComplexNum operator-(const ComplexNum &) const;
+    ComplexNum operator*(const ComplexNum &) const;
+    ComplexNum operator/(const ComplexNum &) const;
+    bool operator==(const ComplexNum &) const;
+    ComplexNum operator<<(const ComplexNum &) const;
+    ComplexNum operator>>(const ComplexNum &) const;
 };
 
 int main()
@@ -18,8 +26,55 @@ int main()
     return 0;
 }
 
-ComplexNum::ComplexNum(const int &input_real, const int &input_imagine = 0)
+ComplexNum::ComplexNum(const double &input_real, const double &input_imagine = 0)
 {
     this->real = input_real;
     this->imagine = input_imagine;
 }
+
+ComplexNum ComplexNum::operator-()
+{
+    return ComplexNum(-this->real, this->imagine);
+}
+
+ComplexNum ComplexNum::operator+(const ComplexNum &other) const
+{
+    double real = this->real + other.real;
+    double imagine = this->imagine + other.imagine;
+    return ComplexNum(real, imagine);
+}
+
+ComplexNum ComplexNum::operator-(const ComplexNum &other) const
+{
+    double real = this->real - other.real;
+    double imagine = this->imagine - other.imagine;
+    return ComplexNum(real, imagine);
+}
+
+ComplexNum ComplexNum::operator*(const ComplexNum &other) const
+{
+    double real = this->real * other.real - this->imagine * other.imagine;
+    double imagine = this->real * other.imagine + this->imagine * other.real;
+    return ComplexNum(real, imagine);
+}
+
+ComplexNum ComplexNum::operator/(const ComplexNum &other) const
+{
+    double real = (this->real * other.real + this->imagine * other.imagine) /
+                  (other.real * other.real + other.imagine * other.imagine);
+    double imagine = (other.real * this->imagine - this->real * other.imagine) /
+                     (other.real * other.real + other.imagine * other.imagine);
+    return ComplexNum(real, imagine);
+}
+
+bool ComplexNum::operator==(const ComplexNum &other) const
+{
+    return ((this->real == other.real) && (this->imagine == other.imagine));
+}
+
+/* ComplexNum ComplexNum::operator<<(const ComplexNum &other) const */
+/* { */
+/* } */
+/* ComplexNum ComplexNum::operator>>(const ComplexNum &other) const */
+/* { */
+/* } */
